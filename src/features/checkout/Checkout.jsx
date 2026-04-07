2c568f56e1ab28f1103d611f922e6efa83dd3dc3
Dawa Lama <https://github.com/dawa10000>
2026-04-07 21:19:06 +0545

    add

--- src/features/checkout/Checkout.jsx
+++ src/features/checkout/Checkout.jsx
@@ -40,8 +40,8 @@ const redirectToEsewa = async (orderId, total) => {
     product_code: productCode,
     product_service_charge: "0",
     product_delivery_charge: "0",
-    success_url: `${baseUrl}/payment-success`,
-    failure_url: `${baseUrl}/payment-failed`,
+    success_url: `${baseUrl}/checkout/verify-esewa`,
+    failure_url: `${baseUrl}/checkout/verify-esewa`,
     signed_field_names: "total_amount,transaction_uuid,product_code",
     signature,
   };
