2cf5e7e0b47c6419ea0a036c1755dfc113bcefa7
Dawa Lama <https://github.com/dawa10000>
2026-04-07 20:24:00 +0545

    add

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { setDefaultResultOrder } from "dns";
+setDefaultResultOrder("ipv4first");
+
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 
@@ -15,8 +18,7 @@ import reviewRoutes from './routes/reviewRoutes.js';
 import checkoutRoutes from './routes/checkoutRoutes.js';
 
 
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
+
 
 const app = express();
 setServers(["1.1.1.1", "8.8.8.8"]);
