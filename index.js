fed86b9d8d67463778b3861d7a42499f9e989d57
Dawa Lama <https://github.com/dawa10000>
2026-04-07 10:51:29 +0545

    add

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.DB_URL).then((val) => {
 
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5173']
+  origin: ['http://localhost:5173', 'https://mern-ecommerce-app-xi.vercel.app']
 }));
 app.use(cookieParser());
 app.use(fileUpload({
