9cc7837a026db7239744346980ca1e60adb106f3
Dawa Lama <https://github.com/dawa10000>
2026-04-08 12:21:03 +0545

    add

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -127,7 +127,7 @@ export const getOrder = async (req, res) => {
 
 export const verifyEsewa = async (req, res) => {
   try {
-    const { data } = req.query;
+    const { data } = req.body;
 
     const decoded = JSON.parse(Buffer.from(data, 'base64').toString('utf-8'));
 
