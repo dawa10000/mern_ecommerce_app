b5700ceff5b8f7ee41932f542ebbd3eeee5c676f
Dawa Lama <https://github.com/dawa10000>
2026-04-09 12:46:44 +0545

    add

--- utlis/mailer.js
+++ utlis/mailer.js
@@ -3,12 +3,17 @@ import User from '../models/User.js';
 import dns from 'dns';
 
 
-dns.setDefaultResultOrder('ipv4first');
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { ...options, family: 4 }, callback);
   },
 });
 
