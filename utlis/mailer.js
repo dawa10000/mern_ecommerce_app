2854adf2b2ca60cc66d2ac929a5bf09b1577f25e
Dawa Lama <https://github.com/dawa10000>
2026-04-09 11:46:20 +0545

    add

--- utlis/mailer.js
+++ utlis/mailer.js
@@ -1,11 +1,12 @@
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import nodemailer from 'nodemailer';
 import User from '../models/User.js';
-import dns from 'dns';
-import env from 'dotenv';
 
-env.config();
 
-dns.setDefaultResultOrder('ipv4first');
+
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
