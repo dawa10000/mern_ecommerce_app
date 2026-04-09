905c0dabf1b25f048cf0ca8d4766517019ff1d29
Dawa Lama <https://github.com/dawa10000>
2026-04-09 14:12:02 +0545

    add

--- utlis/mailer.js
+++ utlis/mailer.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 export const sendOrderConfirmedCustomer = async (order) => {
