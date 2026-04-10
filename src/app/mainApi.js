9c9dfdc2208364bd1a11be85be75021938c1454e
Dawa Lama <https://github.com/dawa10000>
2026-04-10 10:30:09 +0545

    Add

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,12 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 
-export const baseUrl = 'http://localhost:5000/api';
-export const base = 'http://localhost:5000';
+// export const baseUrl = 'http://localhost:5000/api';
+// export const base = 'http://localhost:5000';
 
 
-// export const baseUrl = 'https://mern-ecommerce-app-2aug.onrender.com/api';
-// export const base = 'https://mern-ecommerce-app-2aug.onrender.com';
+export const baseUrl = 'https://mern-ecommerce-app-2aug.onrender.com/api';
+export const base = 'https://mern-ecommerce-app-2aug.onrender.com';
 
 export const mainApi = createApi({
   reducerPath: 'mainApi',
