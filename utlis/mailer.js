274d100cc33af02e715b647b26741bb29b41356e
Dawa Lama <https://github.com/dawa10000>
2026-05-09 12:21:57 +0545

    add

--- utlis/mailer.js
+++ utlis/mailer.js
@@ -8,8 +8,10 @@ dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
+  // port: 587,
+  // secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
